f371dc72dfd74c6263ec372e59ddd6862c1bcd92
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2025-05-08 15:51:35 +0700

    Update login header to improve clarity and branding

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -42,11 +42,15 @@ export default function Page() {
         <div className="flex h-screen w-screen items-center justify-center bg-background bg-gradient-to-br from-green-400 to-blue-500">
             <div className="w-full max-w-md overflow-hidden rounded-2xl flex flex-col gap-8">
                 <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
-                    <h1 className="text-5xl font-light dark:text-zinc-50 text-green-800">LOGIN TITIK</h1>
-                    <Image
+                    <h1 className="text-5xl font-light dark:text-zinc-50 text-green-800">LOGIN</h1>
+                    {/* <Image
                         src={bpjsLogo}
                         alt="Picture of the author"
-                    />
+                    /> */}
+
+                    <p className="text-6xl font-bold" style={{ fontFamily: "'Comfortaa', cursive" }}>
+                        <span className="text-[#229753]">T.I.T.I.K.</span><span className="text-[#FFA76C]"></span>
+                    </p>
                 </div>
                 <AuthForm action={handleSubmit} defaultEmail={email}>
                     <SubmitButton>Login</SubmitButton>
